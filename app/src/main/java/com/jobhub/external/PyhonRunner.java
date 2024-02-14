0c432fc9f7122b54a848dd7a65cbe2ed1400bb01
yunju2 <sikka2034@gmail.com>
2024-02-15 00:41:43 +0900

    add python crawling with process builder

--- app/src/main/java/com/jobhub/external/PyhonRunner.java
+++ app/src/main/java/com/jobhub/external/PyhonRunner.java
@@ -1,36 +1,87 @@
 package com.jobhub.external;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 
+@Slf4j
 @Component
 public class PyhonRunner {
-    public void runPythonScript() {
 
-        String pythonScriptPath = "../crawling/src/main/python/crawl_job_website.py";
-        String pythonVirtualEnvPath = "../crawling/venv/bin/python3";
-        ProcessBuilder processBuilder =
-                new ProcessBuilder(
-                        pythonVirtualEnvPath,
-                        pythonScriptPath
-                );
+    public void runPythonScript() {
+        checkAndInstallRequiredPackages();
         try {
-            Process process = processBuilder.start();
-            System.out.println("Started");
-            process.getOutputStream().close();
-            System.out.println("Closed");
-            StringWriter output = new StringWriter();
-            try(BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                System.out.println("Buffer created");
-                reader.transferTo(output);
+            Process pythonProcess = runPythonCrawling();
+            handlePythonProcess(pythonProcess);
+        } catch (IOException | InterruptedException e) {
+            log.error("Error Occur When Python Crawling", e);
+        }
+    }
+
+    private Process runPythonCrawling() throws IOException, InterruptedException {
+        ProcessBuilder pythonBuilder = new ProcessBuilder(
+                "python3","../crawling/src/main/python/crawl_job_website.py"
+        );
+        Process pythonProcess = pythonBuilder.start();
+        log.debug("Python Crawling Process Started");
+        pythonProcess.waitFor();
+        log.debug("Python Crawling Process Finished");
+        return pythonProcess;
+    }
+
+    private void handlePythonProcess(Process pythonProcess) throws IOException {
+        StringWriter output = readPythonProcessOutput(pythonProcess);
+        if (output.toString().contains("Finished")) {
+            log.info("Python Crawling Process Success");
+        } else {
+            logPythonProcessErrors(pythonProcess);
+        }
+    }
+
+    private StringWriter readPythonProcessOutput(Process pythonProcess) throws IOException {
+        StringWriter output = new StringWriter();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(pythonProcess.getInputStream()))) {
+            log.debug("Result Buffer created");
+            reader.transferTo(output);
+        }
+        return output;
+    }
+
+    private void logPythonProcessErrors(Process pythonProcess) throws IOException {
+        StringWriter errorOutput = new StringWriter();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(pythonProcess.getErrorStream()))) {
+            log.debug("Result Buffer created");
+            reader.transferTo(errorOutput);
+        }
+        log.error("Python Crawling Process Error: " + errorOutput);
+    }
+
+    private void checkAndInstallRequiredPackages() {
+        checkAndInstallPythonPackage("selenium");
+        checkAndInstallPythonPackage("requests");
+    }
+
+    private void checkAndInstallPythonPackage(String packageName) {
+        try {
+            // Check if selenium is installed
+            ProcessBuilder checkBuilder = new ProcessBuilder();
+            checkBuilder.command("pip", "show", packageName);
+            Process checkProcess = checkBuilder.start();
+            int exitCode = checkProcess.waitFor();
+
+            // If selenium is not installed, install it
+            if (exitCode != 0) {
+                ProcessBuilder installBuilder = new ProcessBuilder();
+                installBuilder.command("pip", "install", packageName);
+                Process installProcess = installBuilder.start();
+                installProcess.waitFor();
             }
-            System.out.println(output);
-            process.waitFor();
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error occurred when checking or installing package {}", packageName, e);
         }
     }
 
--- app/src/main/java/com/jobhub/scheduler/RecruitmentCrawlingScheduler.java
+++ app/src/main/java/com/jobhub/scheduler/RecruitmentCrawlingScheduler.java
@@ -1,13 +1,18 @@
 package com.jobhub.scheduler;
 
+import com.jobhub.external.PyhonRunner;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class RecruitmentCrawlingScheduler {
 
+    private final PyhonRunner pyhonRunner;
+
     @Scheduled(cron = "0 0 12 * * ?")
     public void crawlingRecruitments() {
-
+        pyhonRunner.runPythonScript();
     }
 }
